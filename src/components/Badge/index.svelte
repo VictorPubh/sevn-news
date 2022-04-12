d45767e92e2fea00c2f115eefe0ffbe24581ef70
João Victor <joaosantana@intnet.com.br>
2022-04-12 02:18:10 -0300

    feat: create Badge component

---
+++ src/components/Badge/index.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+    export let category: string;
+    import { categoriesColors } from "../../utils/colors"
+</script>
+
+<em class="badge" style="--badge-color: {categoriesColors[category]}">
+    {category}
+</em>
+
+<style>
+    .badge {
+        font-family: 'IBM Plex Sans';
+        font-style: normal;
+        font-weight: 700;
+        font-size: 15px;
+        color: var(--badge-color);
+    }
+</style>
