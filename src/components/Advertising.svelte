4e35d6df47dee0a7108c9a9edbd8f50087e1eef9
João Victor <joaosantana@intnet.com.br>
2022-04-12 02:15:42 -0300

    feat: create Advertising component

---
+++ src/components/Advertising.svelte
@@ -0,0 +1,25 @@
+<script>
+    const text = "Publicidade"
+</script>
+
+<div class="advertising">
+    {text}
+</div>
+
+<style>
+    .advertising {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        margin: 32px auto;
+
+        background-color: #EBEFF1;
+        width: 100%;
+        height: 12rem;
+        border-radius: 10px;
+
+        color: rgba(0, 0, 0, 0.2);
+        font-size: 2rem;
+        font-weight: 700;
+    }
+</style>
