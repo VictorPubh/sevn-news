431ce41435dc4bda60dcbb0be15111afe47197b0
João Victor <joaosantana@intnet.com.br>
2022-04-12 02:21:54 -0300

    feat: create article/:ID page

---
+++ src/routes/article/[id].svelte
@@ -0,0 +1,79 @@
+<script>
+    import Header from "../../components/Header.svelte"
+    import Badge from "../../components/Badge/index.svelte"
+    import Footer from '../../components/Footer.svelte'
+    import Advertising from "../../components/Advertising.svelte";
+
+    export let article;
+
+    $: articleDate = `${article.date}, Por: ${article.author}`
+    $: articleBody = article.article
+</script>
+
+<script context="module">
+    /** @type {import('./[slug]').Load} */
+    export async function load({ params, fetch, session, stuff }) {
+        const url = `https://apimock.sevn.technology/news/articles/${params.id}`;
+        const response = await fetch(url);
+
+        return {
+            status: response.status,
+            props: {
+                article: await response.json()
+            }
+        };
+    }
+</script>
+
+<Header buttonReturn />
+
+<div class="full-article container">
+    <div class="article-badge">
+        <Badge category={article.category} />
+    </div>
+    <h1>{article.title}</h1>
+    <p>{article.subtitle}</p>
+    <sub>{articleDate}</sub>
+    <Advertising full />
+    <article>
+        {@html articleBody}
+    </article>
+</div>
+
+<Footer />
+
+<style>
+    .full-article {
+        margin: auto;
+    }
+
+    .full-article .article-badge {
+        margin: 4rem 0 1rem 0;
+    }
+
+    .full-article h1 {
+        font-weight: 700;
+        font-size: 58px;
+        line-height: 60px;
+    }
+
+    .full-article p {
+        margin: 2rem 0;
+        font-weight: 400;
+        font-size: 25px;
+        line-height: 32px;
+    }
+
+    .full-article sub {
+        font-weight: 700;
+        font-size: 1rem;
+        line-height: 16px;
+        color: rgba(0, 0, 0, 0.2);
+    }
+
+    .full-article article {
+        font-weight: 400;
+        font-size: 1.5rem;
+        line-height: 1.75rem;
+    }
+</style>
